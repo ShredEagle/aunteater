dcfd2912bcfe38ab61cbac9959a945fa87b2665f
Francois Poizat <francois.poizat@gmail.com>
2021-10-12 10:56:23 +0200

    adds a templatization of the Timer type for more generic system

--- src/libs/aunteater/aunteater/System.h
+++ src/libs/aunteater/aunteater/System.h
@@ -8,19 +8,19 @@ namespace aunteater
 
 class EntityManager;
 
-template <class T_inputState = void>
+template <class T_timer = Timer, class T_inputState = void>
 class System
 {
 public:
-    virtual void update(const Timer aTime, const T_inputState & aInputState) = 0;
+    virtual void update(const T_timer aTime, const T_inputState & aInputState) = 0;
 };
 
 /// \brief Specialization when there are no input state provided for the update()
-template <>
-class System<void>
+template <class T_timer>
+class System<T_timer, void>
 {
 public:
-    virtual void update(const Timer aTime) = 0;
+    virtual void update(const T_timer aTime) = 0;
 };
 
 } // namespace aunteater
